478420332e69d7f12d627f7a9514af6f2b678ea6
TimSchrody <115226610+TimSchrody@users.noreply.github.com>
2023-04-28 11:32:05 +0200

    added scripts

---
+++ views/Profile/SqlProfile.js
@@ -0,0 +1,40 @@
+console.log(3);
+
+const playerID = 1;
+
+//var mysql = require('mysql2');
+
+//import * as mysql from '.../node_modules/mysql2';
+
+//import fs from 'node:fs/promises'; 
+
+//import * as mysql from 'node:mysql2';
+
+
+//import * as mysql from 'mysql2/promise';
+
+//console.log(mysql);
+
+//import {createConnection, QueryError, RowDataPacket} from 'mysql2';
+
+/*
+import test from '/Profile/test.js';
+
+console.log(test);
+
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "paulCC6763!"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+var abzufragende_user = 10;
+var playerStats = [];
+
+*/
