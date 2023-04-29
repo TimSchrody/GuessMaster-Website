b7b5a46201d6272c2faa1ca05f023f6772714bc9
Bonddied123 <Marius2711@gmx.de>
2023-04-29 18:14:09 +0200

    Profile Cursor Fix

--- views/Profile/profile.js
+++ views/Profile/profile.js
@@ -5,5 +5,36 @@ function LoadData(){
 
     let username = document.getElementById("username1").innerHTML;
 
+    console.log(username);
+
 }
 
+
+function ShowMatchHistory(){
+    //to be added
+    console.log("Test History");
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+function cursorBig(){
+    document.querySelector(".cursor").classList.toggle("increase");
+    document.querySelector(".cursor2").classList.toggle("increase");
+}
+function cursorSmall(){
+    document.querySelector(".cursor").classList.toggle("increase");
+    document.querySelector(".cursor2").classList.toggle("increase");
+}
+
+const cursor = document.querySelector(".cursor");
+document.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY+ "px";
+});
+
+const cursor2 = document.querySelector(".cursor2");
+document.addEventListener("mousemove", (e) => {
+    cursor2.style.left = e.clientX + "px";
+    cursor2.style.top = e.clientY+ "px";
+});
