7e02e323b8fa7b165c95b229ce65a6dd1b660d35
TimSchrody <115226610+TimSchrody@users.noreply.github.com>
2023-04-30 16:58:25 +0200

    updated profile css

--- views/Profile/profile.js
+++ views/Profile/profile.js
@@ -173,3 +173,13 @@ document.addEventListener("mousemove", (e) => {
     cursor2.style.left = e.clientX + "px";
     cursor2.style.top = e.clientY+ "px";
 });
+
+const header = document.querySelector(".header");
+window.addEventListener("scroll", function(){
+    if(window.scrollY==0){
+      //user is at the top of the page; no need to show the back to top button
+        header.classList.remove("scroll")
+    } else {
+        header.classList.add("scroll")
+    }
+});
